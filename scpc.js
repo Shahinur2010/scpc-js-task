bc4b66432cb1ab5901955223e4e5aa0c8969d2c5
Shahinur2010 <shahinurmony4@gmail.com>
2023-07-27 18:47:23 +0600

    problem-6 solved

--- scpc.js
+++ scpc.js
@@ -112,3 +112,61 @@ const inputArray = [1, 3, 6, 8, 11, 15];
 const targetValue = 9;
 const result = findTwoNumbersWithSum(inputArray, targetValue);
 console.log(result); 
+
+/* ............................................................................ */
+
+// Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+// program for a simple calculator
+
+// take the operator input
+const operator =  ('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(('Enter first number: '));
+const number2 = parseFloat(('Enter second number: '));
+
+let res;
+
+// using if...else if... else
+if (operator == '+') {
+    res = number1 + number2;
+}
+else if (operator == '-') {
+    res = number1 - number2;
+}
+else if (operator == '*') {
+    res = number1 * number2;
+}
+else {
+    res = number1 / number2;
+}
+
+// display the res
+// console.log(`${3.5} ${*} ${5.6} = ${res}`);
+
+
+/* ............................................................................ */
+// Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+function generateRandomPassword(length) {
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numberChars = '0123456789';
+  const specialChars = '!@#$%^&*()_-+=<>?/{}~|';
+
+  const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Example usage: Generate a random password of length 12
+const passwordLength = 12;
+const randomPassword = generateRandomPassword(passwordLength);
+console.log(randomPassword);
